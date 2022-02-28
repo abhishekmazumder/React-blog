776d7dbc48668cf67acc9501fdcb6bcf685a2ed1
abhishek <abhishekmazumder69@gmail.com>
2022-02-28 21:18:40 +0530

    WritePost Page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
 import SinglePostPage from './pages/singlePostPage/SinglePostPage';
+import WritePost from './pages/WritePost/WritePost';
 
 function App() {
 	return (
 		<div>
 			<Topbar />
 			{/* <Home /> */}
-			<SinglePostPage />
+			{/* <SinglePostPage /> */}
+			<WritePost />
 		</div>
 	);
 }
---
+++ src/pages/WritePost/WritePost.jsx
@@ -0,0 +1,38 @@
+import { FaPlus, FaCloudUploadAlt } from 'react-icons/fa';
+import './writePost.css';
+
+const WritePost = () => {
+	return (
+		<div className="write">
+			<img
+				src="https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+				alt=""
+				className="writeImg"
+			/>
+			<form className="writeForm">
+				<div className="writeFormGroup">
+					<label htmlFor="fileInput">
+						<FaPlus className="writeIcon" />
+					</label>
+					<input type="file" id="fileInput" style={{ display: 'none' }} />
+					<input
+						type="text"
+						placeholder="Title"
+						className="writeInput"
+						autoFocus={true}
+					/>
+				</div>
+				<div className="writeFormGroup">
+					<textarea
+						placeholder="Write your story..."
+						type="text"
+						className="writeInput writeText"
+					></textarea>
+				</div>
+				<button className="writeSubmit">Publish</button>
+			</form>
+		</div>
+	);
+};
+
+export default WritePost;
